681cbfdf1d02c6ae276715722b7d67d18da1c78c
HaiyangLiu1997 <41906591+HaiyangLiu1997@users.noreply.github.com>
2019-09-26 12:30:36 +0900

    Update CMUnet.py

--- network/openpose/CMUnet.py
+++ network/openpose/CMUnet.py
@@ -16,7 +16,7 @@ def cli(parser):
     group = parser.add_argument_group('network')
     group.add_argument('--heatmap_num', default=19, type=int)
     group.add_argument('--paf_num', default=38, type=int)
-    group.add_argument('--paf_stage', default=5, type=int)
+    group.add_argument('--paf_stage', default=4, type=int)
     group.add_argument('--weight_vgg19', default='https://download.pytorch.org/models/vgg19-dcbb9e9d.pth')
     
 class CMUnetwork(nn.Module):
@@ -26,12 +26,12 @@ class CMUnetwork(nn.Module):
         # already finish the init_weight in each block
         super(CMUnetwork, self).__init__()
         self.state_0 = VGG_block()
-        self.state_1 = state_n_block(128, args.paf_num)
+        self.state_1 = state_1_block(128, args.paf_num)
         self.state_2 = state_n_block(128+args.paf_num,args.paf_num)
         self.state_3 = state_n_block(128+args.paf_num,args.paf_num)
         self.state_4 = state_n_block(128+args.paf_num,args.paf_num)
-        self.state_5 = state_n_block(128+args.paf_num,args.paf_num)
-        self.state_6 = state_n_block(128+args.paf_num,args.heatmap_num)
+        self.state_5 = state_1_block(128+args.paf_num,args.heatmap_num)
+        self.state_6 = state_n_block(128+args.heatmap_num,args.heatmap_num)
 
     def forward(self,input_0):
 
@@ -72,16 +72,59 @@ class dense_block(nn.Module):
 
     def __init__(self, in_dim, out_dim):
         super(dense_block, self).__init__()
-        # default inplace = False for ReLU
-        self.conv1 = nn.Sequential( nn.Conv2d(in_dim, 128, 1, 1, 0),
-                                    nn.Conv2d(128, 128, 3, 1, 1),
-                                    nn.ReLU(inplace=True))
+        # default inplace = False for PReLU
+        
+        self.conv1 = nn.Sequential( #nn.Conv2d(in_dim, 128, 1, 1, 0),
+                                    nn.Conv2d(in_dim, 128, 3, 1, 1),
+                                    nn.PReLU(num_parameters=128))
         self.conv2 = nn.Sequential(nn.Conv2d(128, 128, 3, 1, 1),
-                                    nn.ReLU(inplace=True))
+                                    nn.PReLU(num_parameters=128))
         # self.conv3 = nn.Sequential(nn.Conv2d(128, (out_dim-256), 3, 1, 1),
-        #                             nn.ReLU(inplace=True))
-        self.conv3 = nn.Sequential(nn.Conv2d(128, 128, 3, 1, 1),nn.ReLU(inplace=True))
-        #self.conv4 = nn.Sequential(nn.Conv2d(384, out_dim, 1, 1, 0),nn.ReLU(inplace=True))
+        #                             nn.PReLU(num_parameters=128))
+        self.conv3 = nn.Sequential(nn.Conv2d(128, 128, 3, 1, 1),nn.PReLU(num_parameters=128))
+        #self.conv4 = nn.Sequential(nn.Conv2d(384, out_dim, 1, 1, 0),nn.PReLU(num_parameters=128))
+        self.initialize_weight()
+        
+
+    def forward(self,input_1):
+        # debug = True
+        output_1 = self.conv1(input_1)
+        output_2 = self.conv2(output_1)
+        output_3 = self.conv3(output_2)
+        output = torch.cat([output_1,output_2,output_3],1)
+        #output_4 = self.conv4(output)
+        # if debug:
+        #     output = output_3
+        # output = torch.cat([output_1,output_2,output_3],1)
+        return output
+    
+    def initialize_weight(self):
+        for m in self.modules():
+            #print('need check init')
+            if isinstance(m, nn.Conv2d):
+                init.normal_(m.weight, std = 0.01)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0.0)
+
+class dense_block_0(nn.Module):
+    '''1. basic dense block of the new openpose
+       2. add conv1,2,3 output together to output
+       3. default kernal_size = 3,bias = true
+    '''
+
+    def __init__(self, in_dim, out_dim):
+        super(dense_block_0, self).__init__()
+        # default inplace = False for PReLU
+        
+        self.conv1 = nn.Sequential( #nn.Conv2d(in_dim, 128, 1, 1, 0),
+                                    nn.Conv2d(in_dim, 96, 3, 1, 1),
+                                    nn.PReLU(num_parameters=96))
+        self.conv2 = nn.Sequential(nn.Conv2d(96, 96, 3, 1, 1),
+                                    nn.PReLU(num_parameters=96))
+        # self.conv3 = nn.Sequential(nn.Conv2d(128, (out_dim-256), 3, 1, 1),
+        #                             nn.PReLU(num_parameters=128))
+        self.conv3 = nn.Sequential(nn.Conv2d(96, 96, 3, 1, 1),nn.PReLU(num_parameters=96))
+        #self.conv4 = nn.Sequential(nn.Conv2d(384, out_dim, 1, 1, 0),nn.PReLU(num_parameters=128))
         self.initialize_weight()
         
 
@@ -105,6 +148,7 @@ class dense_block(nn.Module):
                 if m.bias is not None:
                     init.constant_(m.bias, 0.0)
     
+    
         
 class state_n_block(nn.Module):
     ''' 1. for state 1 in_dim = 128
@@ -120,7 +164,7 @@ class state_n_block(nn.Module):
         self.block4 = dense_block(384,128)
         self.block5 = dense_block(384,128)
         self.conv1  = nn.Sequential(nn.Conv2d(384, 512, 1, 1, 0),
-                                     nn.ReLU(inplace = True))
+                                     nn.PReLU(num_parameters=512))
         self.conv2  = nn.Conv2d(512,out_dim,1,1,0)
         self.initialize_weight()
 
@@ -143,10 +187,47 @@ class state_n_block(nn.Module):
         init.normal_(self.conv2.weight, std =0.01)
         init.constant_(self.conv2.bias, 0.0)
 
+class state_1_block(nn.Module):
+    ''' 1. for state 1 in_dim = 128
+        2. for other state depend on the paf and heatmap channels
+    '''
+
+    def __init__(self, in_dim, out_dim):
+        # 384 = 128 *3
+        super(state_1_block, self).__init__()
+        self.block1 = dense_block_0(in_dim,288)
+        self.block2 = dense_block_0(288,96)
+        self.block3 = dense_block_0(288,96)
+        self.block4 = dense_block_0(288,96)
+        self.block5 = dense_block_0(288,96)
+        self.conv1  = nn.Sequential(nn.Conv2d(288, 256, 1, 1, 0),
+                                        nn.PReLU(num_parameters=256))
+        self.conv2  = nn.Conv2d(256,out_dim,1,1,0)
+        self.initialize_weight()
+
+    def forward(self,input_1):
+        ''' inplace the midresult '''
+        output_1 = self.block1(input_1)
+        output_1 = self.block2(output_1)
+        output_1 = self.block3(output_1)                     
+        output_1 = self.block4(output_1)
+        output_1 = self.block5(output_1)
+        output_1 = self.conv1(output_1)
+        output_1 = self.conv2(output_1)
+        return output_1
+
+    def initialize_weight(self):
+        '''init 1*1 conv block
+        '''
+        init.normal_(self.conv1[0].weight, std =0.01)
+        init.constant_(self.conv1[0].bias, 0.0)
+        init.normal_(self.conv2.weight, std =0.01)
+        init.constant_(self.conv2.bias, 0.0)
+
 
 class VGG_block(nn.Module):
     ''' 1. default have the bias
-        2. using relu and 3 * max pooling
+        2. using PReLU and 3 * max pooling
         3. 10 layers of VGG original
         4. 2 extra layers by CMU
         5. default in_dim = 3,out_dim = 128
